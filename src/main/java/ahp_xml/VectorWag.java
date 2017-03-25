0f14f76f3f79f6b3bfca49f51cd564300830fe48
Ye Yevv <Ye Yevv>
2017-03-25 15:45:00 +0100

    v2.0 Project created :D

---
+++ src/main/java/ahp_xml/VectorWag.java
@@ -0,0 +1,51 @@
+package ahp_xml;
+
+import java.util.Arrays;
+
+/**
+ * Created by EwaStachow on 15/03/2017.
+ */
+
+public class VectorWag {
+
+    double[] vector;
+    double wag;
+
+    public VectorWag(double[] vector, double wag) {
+        this.vector = vector;
+        this.wag = wag;
+    }
+
+    public VectorWag() {
+    }
+
+    @Override
+    public String toString() {
+        return "VectorWag{" +
+                "vector=" + Arrays.toString(vector) +
+                ", wag=" + wag +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VectorWag vectorWag = (VectorWag) o;
+
+        if (Double.compare(vectorWag.wag, wag) != 0) return false;
+        return Arrays.equals(vector, vectorWag.vector);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = Arrays.hashCode(vector);
+        temp = Double.doubleToLongBits(wag);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
