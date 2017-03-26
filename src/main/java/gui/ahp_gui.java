5dfcc783c63fc57cbc1d26642f6a92e444f32f48
Ye Yevv <Ye Yevv>
2017-03-26 15:37:01 +0200

    v2.3 Gui started

---
+++ src/main/java/gui/ahp_gui.java
@@ -0,0 +1,18 @@
+package gui;/**
+ * Created by yevvy on 26/03/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ahp_gui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
